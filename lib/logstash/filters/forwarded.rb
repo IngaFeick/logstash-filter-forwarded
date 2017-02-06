47e6687a1ac6a0cd7c6bc6667fa52b4d11b2afff
Inga Feick <inga.feick@trivago.com>
2017-02-06 18:13:53 +0100

    Initial checkin

---
+++ lib/logstash/filters/forwarded.rb
@@ -0,0 +1,103 @@
+# encoding: utf-8
+require "logstash/filters/base"
+require "logstash/namespace"
+require "ipaddr"
+
+java_import "java.net.InetAddress"
+
+
+# The forwarded filter extracts the client ip from a list of ip adresses. The client ip might be at any position in the list, since not every x-forwarded-for header comes in the correct ordering.
+# It adds two new fields to the event:
+#  - client_ip : string
+#  - proxies : string[]
+
+class LogStash::Filters::Forwarded < LogStash::Filters::Base
+  config_name "forwarded"
+
+  
+  # The field containing the x-forwarded-for string
+  config :source, :validate => :string, :required => true
+
+  # list of ip patterns that private ips start with. 
+  config :private_ip_prefixes, :validate => :array, :required => false, :default => ["10/8", "192.168/16" ,"172.16.0.0/12"]
+  
+  public
+  def register    
+    @private_ips = @private_ip_prefixes.collect do | adress |
+      begin
+        IPAddr.new(adress)
+      rescue ArgumentError => e
+        @logger.warn("Invalid IP network, skipping", :adress => adress)
+        nil
+       end
+    end
+    @private_ips.compact!
+  end # def register
+
+  public
+  def filter(event)
+    return unless filter?(event)
+
+    begin
+      forwarded = event.get(@source)
+      client_ip, proxies = analyse(forwarded)
+      if client_ip
+        event.set("client_ip", client_ip)
+        event.set("proxies", proxies)
+        filter_matched(event)     
+      end
+    rescue Exception => e
+      @logger.error("Unknown error while looking up GeoIP data", :exception => e, :field => @source, :event => event)
+      # Dont' swallow this, bubble up for unknown issue
+      raise e
+    end # begin
+  end # def filter
+
+  def analyse(ip)
+    if ip.nil?
+      return nil, nil
+    end
+
+    if ip.is_a? Array
+      ip_list = ip
+    else
+      ip_list = ip.split(",")      
+    end
+
+    client_ip = get_client_ip(ip_list)
+
+    if ip_list.nil?
+      proxies = [] 
+    else 
+      proxies = ip_list- [client_ip]
+    end
+
+    return client_ip, proxies
+
+  end # def analyse
+
+  def get_client_ip(ip_array)
+    ip_array.each do | ip |
+      ip = ip.strip
+      if !is_private(ip)
+        return ip
+      end # if
+    end # each
+    nil
+  end # get_client_ip
+
+  def is_private(ip)
+    begin
+      ipo = IPAddr.new(ip)
+      @private_ips.each do | private_ip |
+        if private_ip.include?(ipo)
+          return true
+        end
+      end # each
+      false
+    rescue => e
+      @logger.error("Couldnt check if ip is private.", :input_data => ip, :exception => e)
+    end # begin
+  end # is_private
+
+end # class LogStash::Filters::Forwarded
---
+++ spec/filters/forwarded_spec.rb
@@ -0,0 +1,111 @@
+# encoding: utf-8
+require "logstash/devutils/rspec/spec_helper"
+require "logstash/filters/forwarded"
+
+
+describe LogStash::Filters::Forwarded do
+
+
+    let(:plugin) { LogStash::Filters::Forwarded.new("source" => "message") }
+    
+    before do
+      plugin.register
+      plugin.filter(event)
+    end
+    
+    context "multiple client ips" do
+
+      let(:event) { LogStash::Event.new(:message => "123.45.67.89,61.160.232.222") }
+      it "should take the first client ip" do
+        expect(event.get("client_ip")).to eq("123.45.67.89")
+        expect(event.get("proxies")).to eq(["61.160.232.222"])
+      end # it
+    end # context
+
+    context "proper x-forwarded-for" do
+
+      let(:event) { LogStash::Event.new(:message => "84.30.67.207, 10.1.2.162") }
+      it "should take the first public ip" do
+        expect(event.get("client_ip")).to eq("84.30.67.207")
+        expect(event.get("proxies")).to eq(["10.1.2.162"])
+      end # it
+    end # context
+
+    context "proper x-forwarded-for with multiple proxies" do
+
+      let(:event) { LogStash::Event.new(:message => "94.254.183.48, 10.1.2.161, 10.1.2.162") }
+      it "should take the first ip" do
+        expect(event.get("client_ip")).to eq("94.254.183.48")
+        expect(event.get("proxies")).to eq(["10.1.2.161","10.1.2.162"])
+      end # it
+    end # context
+
+    context "proper x-forwarded-for with multiple proxies, no whitespace" do
+
+      let(:event) { LogStash::Event.new(:message => "51.174.213.194,10.1.2.100,10.1.2.83") }
+      it "should take the first client ip" do
+        expect(event.get("client_ip")).to eq("51.174.213.194")
+        expect(event.get("proxies")).to eq(["10.1.2.100","10.1.2.83"])
+      end # it
+    end # context
+
+    context "single client ip" do
+
+      let(:event) { LogStash::Event.new(:message => "185.22.141.112") }
+      it "should set the client ip and leave the proxy list empty" do
+        expect(event.get("client_ip")).to eq("185.22.141.112")
+        expect(event.get("proxies")).to eq([])
+      end # it
+    end # context
+
+    context "single proxy ip" do
+
+      let(:event) { LogStash::Event.new(:message => "10.1.2.162") }
+      it "should set the proxy list and leave the client ip empty" do
+        expect(event.get("client_ip")).to eq(nil)
+        expect(event.get("proxies")).to eq(["10.1.2.162"])
+      end # it
+    end # context
+
+    context "multiple proxy ips" do
+
+      let(:event) { LogStash::Event.new(:message => "10.1.2.162, 10.1.3.255") }
+      it "should set the proxy list and leave the client ip empty" do
+        expect(event.get("client_ip")).to eq(nil)
+        expect(event.get("proxies")).to eq(["10.1.2.162","10.1.3.255"])
+      end # it
+    end # contex
+
+    context "empty message" do
+
+      let(:event) { LogStash::Event.new(:message => "") }
+      it "should return empty or nil values" do
+        expect(event.get("client_ip")).to eq(nil)
+        expect(event.get("proxies")).to eq(nil)
+      end # it
+    end # contex
+
+    context "multiple ips in wrong order" do
+
+      let(:event) { LogStash::Event.new(:message => "10.144.80.56, 82.132.186.219") }
+      it "should take the client ip from the right end of the list" do
+        expect(event.get("client_ip")).to eq("82.132.186.219")
+        expect(event.get("proxies")).to eq(["10.144.80.56"])
+      end # it
+    end # contex
+
+    context "ipv6 client ip" do
+
+      let(:event) { LogStash::Event.new(:message => "2405:204:828e:fa5a::e64:38a5, 64.233.173.148") }
+      it "should take the client ip from the right end of the list" do
+        expect(event.get("client_ip")).to eq("2405:204:828e:fa5a::e64:38a5")
+        expect(event.get("proxies")).to eq(["64.233.173.148"])
+      end # it
+    end # contex
+
+# TODO add more use cases for ipv6 and for the bug report from the github ticket
+#[2017-02-06T18:02:45,832][WARN ][logstash.filters.forwarded] Invalid IP network, skipping {:adress=>"10/8"}
+#[2017-02-06T18:02:45,838][WARN ][logstash.filters.forwarded] Invalid IP network, skipping {:adress=>"192.168/16"}
+
+
+end
