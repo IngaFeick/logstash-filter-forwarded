b1459ad38287bce688fd29621e570f416aa56466
Inga Feick <inga.feick@trivago.com>
2017-02-07 13:08:19 +0100

    Rename event fields to match default names

--- spec/filters/forwarded_spec.rb
+++ spec/filters/forwarded_spec.rb
@@ -17,8 +17,8 @@ describe LogStash::Filters::Forwarded do
 
       let(:event) { LogStash::Event.new(:message => "123.45.67.89,61.160.232.222") }
       it "should take the first client ip" do
-        expect(event.get("client_ip")).to eq("123.45.67.89")
-        expect(event.get("proxies")).to eq(["61.160.232.222"])
+        expect(event.get("forwarded_client_ip")).to eq("123.45.67.89")
+        expect(event.get("forwarded_proxy_list")).to eq(["61.160.232.222"])
       end # it
     end # context
 
@@ -26,8 +26,8 @@ describe LogStash::Filters::Forwarded do
 
       let(:event) { LogStash::Event.new(:message => "84.30.67.207, 10.1.2.162") }
       it "should take the first public ip" do
-        expect(event.get("client_ip")).to eq("84.30.67.207")
-        expect(event.get("proxies")).to eq(["10.1.2.162"])
+        expect(event.get("forwarded_client_ip")).to eq("84.30.67.207")
+        expect(event.get("forwarded_proxy_list")).to eq(["10.1.2.162"])
       end # it
     end # context
 
@@ -35,8 +35,8 @@ describe LogStash::Filters::Forwarded do
 
       let(:event) { LogStash::Event.new(:message => "94.254.183.48, 10.1.2.161, 10.1.2.162") }
       it "should take the first ip" do
-        expect(event.get("client_ip")).to eq("94.254.183.48")
-        expect(event.get("proxies")).to eq(["10.1.2.161","10.1.2.162"])
+        expect(event.get("forwarded_client_ip")).to eq("94.254.183.48")
+        expect(event.get("forwarded_proxy_list")).to eq(["10.1.2.161","10.1.2.162"])
       end # it
     end # context
 
@@ -44,8 +44,8 @@ describe LogStash::Filters::Forwarded do
 
       let(:event) { LogStash::Event.new(:message => "51.174.213.194,10.1.2.100,10.1.2.83") }
       it "should take the first client ip" do
-        expect(event.get("client_ip")).to eq("51.174.213.194")
-        expect(event.get("proxies")).to eq(["10.1.2.100","10.1.2.83"])
+        expect(event.get("forwarded_client_ip")).to eq("51.174.213.194")
+        expect(event.get("forwarded_proxy_list")).to eq(["10.1.2.100","10.1.2.83"])
       end # it
     end # context
 
@@ -53,8 +53,8 @@ describe LogStash::Filters::Forwarded do
 
       let(:event) { LogStash::Event.new(:message => "185.22.141.112") }
       it "should set the client ip and leave the proxy list empty" do
-        expect(event.get("client_ip")).to eq("185.22.141.112")
-        expect(event.get("proxies")).to eq([])
+        expect(event.get("forwarded_client_ip")).to eq("185.22.141.112")
+        expect(event.get("forwarded_proxy_list")).to eq([])
       end # it
     end # context
 
@@ -62,8 +62,8 @@ describe LogStash::Filters::Forwarded do
 
       let(:event) { LogStash::Event.new(:message => "10.1.2.162") }
       it "should set the proxy list and leave the client ip empty" do
-        expect(event.get("client_ip")).to eq(nil)
-        expect(event.get("proxies")).to eq(["10.1.2.162"])
+        expect(event.get("forwarded_client_ip")).to eq(nil)
+        expect(event.get("forwarded_proxy_list")).to eq(["10.1.2.162"])
       end # it
     end # context
 
@@ -71,8 +71,8 @@ describe LogStash::Filters::Forwarded do
 
       let(:event) { LogStash::Event.new(:message => "10.1.2.162, 10.1.3.255") }
       it "should set the proxy list and leave the client ip empty" do
-        expect(event.get("client_ip")).to eq(nil)
-        expect(event.get("proxies")).to eq(["10.1.2.162","10.1.3.255"])
+        expect(event.get("forwarded_client_ip")).to eq(nil)
+        expect(event.get("forwarded_proxy_list")).to eq(["10.1.2.162","10.1.3.255"])
       end # it
     end # contex
 
@@ -80,8 +80,8 @@ describe LogStash::Filters::Forwarded do
 
       let(:event) { LogStash::Event.new(:message => "") }
       it "should return empty or nil values" do
-        expect(event.get("client_ip")).to eq(nil)
-        expect(event.get("proxies")).to eq(nil)
+        expect(event.get("forwarded_client_ip")).to eq(nil)
+        expect(event.get("forwarded_proxy_list")).to eq(nil)
       end # it
     end # contex
 
@@ -89,8 +89,8 @@ describe LogStash::Filters::Forwarded do
 
       let(:event) { LogStash::Event.new(:message => "10.144.80.56, 82.132.186.219") }
       it "should take the client ip from the right end of the list" do
-        expect(event.get("client_ip")).to eq("82.132.186.219")
-        expect(event.get("proxies")).to eq(["10.144.80.56"])
+        expect(event.get("forwarded_client_ip")).to eq("82.132.186.219")
+        expect(event.get("forwarded_proxy_list")).to eq(["10.144.80.56"])
       end # it
     end # contex
 
@@ -98,8 +98,8 @@ describe LogStash::Filters::Forwarded do
 
       let(:event) { LogStash::Event.new(:message => "2405:204:828e:fa5a::e64:38a5, 64.233.173.148") }
       it "should take the client ip from the right end of the list" do
-        expect(event.get("client_ip")).to eq("2405:204:828e:fa5a::e64:38a5")
-        expect(event.get("proxies")).to eq(["64.233.173.148"])
+        expect(event.get("forwarded_client_ip")).to eq("2405:204:828e:fa5a::e64:38a5")
+        expect(event.get("forwarded_proxy_list")).to eq(["64.233.173.148"])
       end # it
     end # contex
 
